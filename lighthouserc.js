8f8e90ba932d7054f5e3535a3a072fb0ed4490a8
Yusuf <38029093+bugnificent@users.noreply.github.com>
2025-03-21 12:13:46 +0300

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './', // Replace with the path to your built files
+      staticDistDir: './html/', // Replace with the path to your built files
     },
     upload: {
       target: 'temporary-public-storage', // Use public storage
