9bd5aa8a20b65139e235bc55ae9b08e6a387d804
Yusuf <38029093+bugnificent@users.noreply.github.com>
2025-04-29 17:43:48 +0300

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      puppeteerScript: './scroll.js',
-      url: ['https://yusufasik.com'],
-      //staticDistDir: './',
+      //url: ['https://yusufasik.com'],
+      staticDistDir: './',
     },
     upload: {
       target: 'temporary-public-storage',
